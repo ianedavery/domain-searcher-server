870660a95ab437b529a6b29c298da35c2736a529
Ian Avery <ianedavery@gmail.com>
2018-05-04 11:54:53 -0700

    node server for calling godaddy api

--- app.js
+++ app.js
@@ -1,59 +1,23 @@
 const express = require('express');
 const logger = require('morgan');
-const mongoose = require('mongoose');
+const request = require('superagent');
+require('dotenv').config();
 
-const {DATABASE_URL, PORT} = require('./config');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./users/router');
-
-//allow mongoose to use ES6 promises
-mongoose.Promise = global.Promise;
+let sso_key = process.env.SSO_KEY;
 
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/:domain', (req, res) => {
+  	request.get(`https://api.ote-godaddy.com/v1/domains/available?domain=${req.params.domain}&checkType=FULL&forTransfer=false/`)    
+    	.set({Accept: 'application/json', Authorization: `sso-key ${sso_key}`})
+    	.then((data) => {
+    		res.json(JSON.parse(data.text));
+    	})
+    	.catch(err => {
+    		res.status(500).json({message: 'Internal Server Error'});
+    	})
+})
 
-let server;
-
-const runServer = (databaseUrl = DATABASE_URL, port = PORT) => {
-	return new Promise((resolve, reject) => {
-		mongoose.connect(databaseUrl, err => {
-			server = app
-				.listen(port, () => {
-					console.log(`Your app is listening on port ${PORT}`);
-					resolve();
-				})
-				.on('error', err => {
-					mongoose.disconnect();
-					reject(err)
-				});
-		});
-	});
-}
-
-const closeServer = () => {
-	return mongoose.disconnect()
-		.then(() => {
-			return new Promise((resolve, reject) => {
-				console.log('closing server');
-				server.close(err => {
-					if(err) {
-						return reject(err);
-					}
-					resolve();
-				});
-			});
-		});
-}
-
-//this is a useful trick for making this file both an executable script, and a module.
-//this will be helpful when we are running tests.
-if(require.main === module) {
-	runServer().catch(err => console.log(err));
-};
-
-module.exports = {app, runServer, closeServer};
+app.listen(8080);
--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-'use strict';
-
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/express-chorescore';
-exports.PORT = process.env.PORT || 8080;
+exports.SSO_KEY = process.env.SSO_KEY;
