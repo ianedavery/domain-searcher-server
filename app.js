898c18063ed606e481aed6192209358d3bf9091b
Ian Avery <ianedavery@gmail.com>
2018-05-05 09:11:46 -0700

    added error messages

--- app.js
+++ app.js
@@ -9,14 +9,24 @@ const app = express();
 
 app.use(logger('dev'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 app.get('/:domain', (req, res) => {
   	request.get(`https://api.ote-godaddy.com/v1/domains/available?domain=${req.params.domain}&checkType=FULL&forTransfer=false/`)    
     	.set({Accept: 'application/json', Authorization: `sso-key ${sso_key}`})
-    	.then((data) => {
+    	.then(data => {
     		res.json(JSON.parse(data.text));
     	})
     	.catch(err => {
-    		res.status(500).json({message: 'Internal Server Error'});
+    		res.status(500).json(JSON.parse(err.response.text));
     	})
 })
 
