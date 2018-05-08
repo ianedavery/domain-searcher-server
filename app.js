dfa47f6589c3ec4f0f660248d18dd915c64ce34d
Ian Avery <ianedavery@gmail.com>
2018-05-08 13:32:23 -0700

    reconfigued app.js

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const logger = require('morgan');
 const request = require('superagent');
 require('dotenv').config();
 
-let sso_key = process.env.SSO_KEY;
-
-
 const app = express();
 
 app.use(logger('dev'));
@@ -22,11 +19,9 @@ app.use(function (req, res, next) {
 
 app.get('/:domain', (req, res) => {
   	request.get(`https://api.ote-godaddy.com/v1/domains/available?domain=${req.params.domain}&checkType=FULL&forTransfer=false/`)    
-    	.set({Accept: 'application/json', Authorization: `sso-key 3mM44UYhVC4J3w_TkTtwSEqWbdt1koSVZPB7S:TkTvzYV3JoQ4U8YzdeBuf2`})
+    	.set({Accept: 'application/json', Authorization: `sso-key ${process.env.SSO_KEY}`})
     	.then(data => {
     		res.json(JSON.parse(data.text));
-            console.log(sso_key);
-
     	})
     	.catch(err => {
     		res.status(500).json(JSON.parse(err.response.text));
