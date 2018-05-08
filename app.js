1b1c6bdcb039a641565cf244bd131f909115e2d9
Ian Avery <ianedavery@gmail.com>
2018-05-08 13:18:13 -0700

    added app.listed

--- app.js
+++ app.js
@@ -34,4 +34,6 @@ app.get('/:domain', (req, res) => {
     	})
 });
 
-//app.listen(8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
